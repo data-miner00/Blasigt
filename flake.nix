a6c53c8899ebe946415587aa57a65ecac4b68911
Shaun Chong <pkay_@live.com>
2024-06-04 08:31:55 +0800

    Add bindings

--- flake.nix
+++ flake.nix
@@ -6,23 +6,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachDefaultSystem (
-  system: let
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    devShells.default =
-      pkgs.mkShell
-        {
-          buildInputs = with pkgs; [
-            dotnet-sdk_8
-          ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        pname = "dotnet-flake";
+        dotnet-sdk = pkgs.dotnet-sdk_8;
+        dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
+        version = "0.0.1";
+      in {
+        devShells.default =
+          pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              dotnet-sdk_8
+            ];
 
-          shellHook = ''
-            echo "Activated Nix Shell"
-          '';
-        };
-  }
-  );
+            shellHook = ''
+              echo "Activated Nix Shell"
+            '';
+          };
+      }
+    );
 }
